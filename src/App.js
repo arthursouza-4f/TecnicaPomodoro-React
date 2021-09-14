4c776d0082cf8dd9c19b727acf796f79a91e2723
arthursouza-4f <joaoanton384@gmail.com>
2021-09-13 22:33:46 -0300

    Adicionado API para o Timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+import "./App.css";
+
+const renderTime = ({ remainingTime }) => {
+  if (remainingTime === 0) {
+    return <div className="text">Acabou.</div>;
+  }
+
+  if (remainingTime >= 60) {
+    remainingTime = (remainingTime / 60) | 0;
+    return (
+      <div className="timer">
+        <div className="text">Faltam</div>
+        <div className="value">{remainingTime}</div>
+        <div className="text">Minutos</div>
+      </div>
+    );
+  }
+
+  if (remainingTime < 60) {
+    return (
+      <div className="timer">
+        <div className="text">Faltam</div>
+        <div className="value">{remainingTime}</div>
+        <div className="text">Segundos</div>
+      </div>
+    );
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="timer-wrapper">
+          <CountdownCircleTimer
+            isPlaying
+            duration={500}
+            colors={[["#FFa500", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+            onComplete={() => [true, 1000]}
+          >
+            {renderTime}
+          </CountdownCircleTimer>
+        </div>
+      </div>
+    </>
   );
 }
 
